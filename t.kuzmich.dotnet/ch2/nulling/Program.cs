5d55048949c8ec4e62787e4fdea925945897aa35
GODELTECH\t.kuzmich <t.kuzmich@godeltech.com>
2021-01-18 14:20:15 +0300

    some progress

--- t.kuzmich.dotnet/ch2/nulling/Program.cs
+++ t.kuzmich.dotnet/ch2/nulling/Program.cs
@@ -4,38 +4,44 @@ using System;
 
 namespace nulling
 {
-    class Program
+  class Program
+  {
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
-        {
-            int thisCannotBeNull = 4;
-            // try
-            // {
-            //     thisCannotBeNull = null;
-            // }
-            // catch (Exception ex)
-            // {
-            //     System.Console.WriteLine(ex.Message);
-            // }
-            int? thisCouldBeNull = null;
-            Console.WriteLine(thisCouldBeNull);
-            Console.WriteLine(thisCouldBeNull.GetValueOrDefault());
-            thisCouldBeNull = 7;
-            Console.WriteLine(thisCouldBeNull);
-            Console.WriteLine(thisCouldBeNull.GetValueOrDefault());
+      int thisCannotBeNull = 4;
+      // try
+      // {
+      //     thisCannotBeNull = null;
+      // }
+      // catch (Exception ex)
+      // {
+      //     System.Console.WriteLine(ex.Message);
+      // }
+      int? thisCouldBeNull = null;
+      Console.WriteLine(thisCouldBeNull);
+      Console.WriteLine(thisCouldBeNull.GetValueOrDefault());
+      thisCouldBeNull = 7;
+      Console.WriteLine(thisCouldBeNull);
+      Console.WriteLine(thisCouldBeNull.GetValueOrDefault());
 
-            var address = new Address();
-            address.Building = null;
-            address.Street = null;
-            address.City = "London";
-            address.Region = null;
-        }
+      var address = new Address();
+      address.Building = null;
+      address.Street = null;
+      address.City = "London";
+      address.Region = null;
+
+      string authorName = null;
+      // the following throws a NullReferenceException
+      int x = authorName.Length;
+      // instead of throwing an exception, null is assigned to y
+      int? y = authorName?.Length;
     }
-    class Address
-    {
-        public string? Building;
-        public string Street;
-        public string City;
-        public string Region;
-    }
+  }
+  class Address
+  {
+    public string? Building;
+    public string Street;
+    public string City;
+    public string Region;
+  }
 }
--- t.kuzmich.dotnet/ch2/numbers/Program.cs
+++ t.kuzmich.dotnet/ch2/numbers/Program.cs
@@ -2,49 +2,49 @@
 
 namespace numbers
 {
-    class Program
+  class Program
+  {
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
-        {
-            int decimalNotation = 2_000_000;
-            int binaryNotation = 0b_0001_1110_1000_0100_1000_0000;
-            int hexadecimalNotation = 0x_001E_8480;
-            Console.WriteLine(decimalNotation);
-            Console.WriteLine(binaryNotation);
-            Console.WriteLine(hexadecimalNotation);
+      int decimalNotation = 2_000_000;
+      int binaryNotation = 0b_0001_1110_1000_0100_1000_0000;
+      int hexadecimalNotation = 0x_001E_8480;
+      Console.WriteLine(decimalNotation);
+      Console.WriteLine(binaryNotation);
+      Console.WriteLine(hexadecimalNotation);
 
-            double a = 0.1;
-            double b = 0.2;
-            if (a + b == 0.3)
-            {
-                Console.WriteLine($"{a} + {b} equals 0.3");
-            }
-            else
-            {
-                Console.WriteLine($"{a} + {b} does NOT equal 0.3");
-            }
+      double a = 0.1;
+      double b = 0.2;
+      if (a + b == 0.3)
+      {
+        Console.WriteLine($"{a} + {b} equals 0.3");
+      }
+      else
+      {
+        Console.WriteLine($"{a} + {b} does NOT equal 0.3");
+      }
 
-            decimal c = 0.1M;
-            decimal d = 0.2M;
-            if (c + d == 0.3M)
-            {
-                Console.WriteLine($"{c} + {d} equals 0.3");
-            }
-            else
-            {
-                Console.WriteLine($"{c} + {d} does NOT equal 0.3");
-            }
+      decimal c = 0.1M;
+      decimal d = 0.2M;
+      if (c + d == 0.3M)
+      {
+        Console.WriteLine($"{c} + {d} equals 0.3");
+      }
+      else
+      {
+        Console.WriteLine($"{c} + {d} does NOT equal 0.3");
+      }
 
-            dynamic aa = "123";
-            System.Console.WriteLine(aa);
-            aa = 12;
-            Console.WriteLine(aa);
+      dynamic aa = "123";
+      System.Console.WriteLine(aa);
+      aa = 12;
+      Console.WriteLine(aa);
 
-            bool happy = true;
-            bool sad = false;
+      bool happy = true;
+      bool sad = false;
 
-            string str = null;
-            System.Console.WriteLine(str);
-        }
+      string str = null;
+      System.Console.WriteLine(str);
     }
+  }
 }
