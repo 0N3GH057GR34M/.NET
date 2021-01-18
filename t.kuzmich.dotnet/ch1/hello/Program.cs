0e52514e776a388ab6929410db301473e6f45ef8
Timur Kuzmich <oneghostgream@gmail.com>
2021-01-18 10:37:08 +0300

    init

---
+++ t.kuzmich.dotnet/ch1/hello/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace hello
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int z = 0;
+            Console.Writeline("Hello, C#!");
+        }
+    }
+}
---
+++ t.kuzmich.dotnet/ch2/numbers/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int decimalNotation = 2_000_000;
+            int binaryNotation = 0b_0001_1110_1000_0100_1000_0000;
+            int hexadecimalNotation = 0x_001E_8480;
+            Console.WriteLine(decimalNotation);
+            Console.WriteLine(binaryNotation);
+            Console.WriteLine(hexadecimalNotation);
+
+            dynamic a = "123";
+            System.Console.WriteLine(a);
+            a = 12;
+            Console.WriteLine(a);
+        }
+    }
+}
