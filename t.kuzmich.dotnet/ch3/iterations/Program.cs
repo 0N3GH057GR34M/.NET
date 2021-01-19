b2c2aea97e781b094244f289bf53d8361c6324e6
GODELTECH\t.kuzmich <t.kuzmich@godeltech.com>
2021-01-19 16:30:27 +0300

    some progress

--- t.kuzmich.dotnet/ch3/iterations/Program.cs
+++ t.kuzmich.dotnet/ch3/iterations/Program.cs
@@ -1,5 +1,8 @@
 ﻿using System;
 using static System.Console;
+using System.IO;
+using System.Collections.Generic;
+using System.Collections;
 
 namespace iterations
 {
@@ -7,7 +10,42 @@ namespace iterations
   {
     static void Main(string[] args)
     {
-      WriteLine("Hello World!");
+      int x = 0;
+      while (x < 10)
+      {
+        Write(x + "\t");
+        x++;
+      }
+      System.Console.WriteLine();
+
+      string password = string.Empty;
+      do
+      {
+        Write("Enter your password: ");
+        password = ReadLine();
+      }
+      while (password != "0000");
+      WriteLine("Correct!");
+
+      for (int y = 1; y <= 10; y++)
+      {
+        Write(y + "\t");
+      }
+      System.Console.WriteLine();
+
+      string[] names = { "Adam", "Barry", "Charlie" };
+      foreach (string name in names)
+      {
+        WriteLine($"{name} has {name.Length} characters.");
+      }
+      System.Console.WriteLine();
+
+      IEnumerator e = names.GetEnumerator();
+      while (e.MoveNext())
+      {
+        string name = (string)e.Current; // Current is read-only!
+        WriteLine($"{name} has {name.Length} characters.");
+      }
     }
   }
 }
