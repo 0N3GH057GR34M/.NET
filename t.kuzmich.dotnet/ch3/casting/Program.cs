ed9b968702d288413cda61b4169ea2b390cfabc8
Tsimur Kuzmich <t.kuzmich@godeltech.com>
2021-01-20 12:21:53 +0300

    some progress

--- t.kuzmich.dotnet/ch3/casting/Program.cs
+++ t.kuzmich.dotnet/ch3/casting/Program.cs
@@ -34,16 +34,8 @@ namespace casting
       object me = new object();
       WriteLine(me.ToString());
 
-      byte[] binaryObject = new byte[8];
-      binaryObject[0] = 255;
-      binaryObject[1] = 255;
-      binaryObject[2] = 255;
-      binaryObject[3] = 255;
-      binaryObject[4] = 255;
-      binaryObject[5] = 255;
-      binaryObject[6] = 255;
-      binaryObject[7] = 255;
-      // (new Random()).NextBytes(binaryObject);
+      byte[] binaryObject = new byte[128];
+      (new Random()).NextBytes(binaryObject);
       WriteLine("Binary Object as bytes:");
       for (int index = 0; index < binaryObject.Length; index++)
       {
@@ -52,6 +44,42 @@ namespace casting
       WriteLine();
       string encoded = Convert.ToBase64String(binaryObject);
       WriteLine($"Binary Object as Base64: {encoded}");
+
+      int age = int.Parse("27");
+      DateTime birthday = DateTime.Parse("4 July 1980");
+      WriteLine($"I was born {age} years ago.");
+      WriteLine($"My birthday is {birthday}.");
+      WriteLine($"My birthday is {birthday:D}");
+
+      Write("How many eggs are there? ");
+      int count;
+      string input = Console.ReadLine();
+      if (int.TryParse(input, out count))
+      {
+        WriteLine($"There are {count} eggs.");
+      }
+      else
+      {
+        WriteLine("I could not parse the input.");
+      }
+
+      WriteLine("Before parsing");
+      Write("What is your age? ");
+      string input1 = Console.ReadLine();
+      try
+      {
+        int age1 = int.Parse(input1);
+        WriteLine($"You are {age1} years old.");
+      }
+      catch (FormatException)
+      {
+        WriteLine("The age you entered is not a valid number format.");
+      }
+      catch (Exception ex)
+      {
+        WriteLine($"{ex.GetType()} says {ex.Message}");
+      }
+      WriteLine("After parsing");
     }
   }
 }
